2b2740b9cb5148688a24337144b0e3f87f16d7ac
Fab Stock <fabstock@stock.eu.org>
2024-09-10 00:29:36 +0200

    - checkout sparse   (not in case) - split job build_* to jest   (to continue)
    - ansible playbook
      todo split playbook and roles
    
    - runner self-hosted
      (same)

---
+++ ansible.sh
@@ -0,0 +1,4 @@
+ ansible-playbook -i ./ansible/hosts2.ini ./ansible/deploy.yml --user=github_ansible_lamanu  -v  
+
+#||  ansible-playbook -i ./ansible/hosts.ini ./ansible/deploy.yml --user=github_ansible_lamanu
+
